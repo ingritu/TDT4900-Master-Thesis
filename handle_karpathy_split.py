3dd679205a61968587682f4d6d3447f3aa82d02e
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2019-10-11 12:55:27 +0200

    moved karpathy split work to project

---
+++ handle_karpathy_split.py
@@ -0,0 +1,65 @@
+import json
+from pathlib import Path
+import pandas as pd
+
+ROOT_PATH = Path(__file__).parent
+
+
+def order_raw_data_and_move_to_interim(data_path, dataset):
+    with open(data_path, 'r') as json_file:
+        data_dict = json.load(json_file)
+
+    train_dict = {}
+    test_dict = {}
+    val_dict = {}
+
+    columns = ['image_id', 'caption_id', 'caption', 'tokens']
+
+    for col in columns:
+        train_dict[col] = []
+        test_dict[col] = []
+        val_dict[col] = []
+    full_dict = {'train': train_dict, 'val': val_dict, 'test': test_dict}
+
+    images = data_dict['images']
+
+    for image in images:
+        # get image name
+        image_id = image['filename']
+        # get split
+        split = image['split']
+        # go through captions and add them to dict split
+        captions = image['sentences']
+        for caption in captions:
+            raw_caption = caption['raw']
+            tokens = caption['tokens']
+            caption_id = image_id[:-4] + '#' + str(caption['sentid'])
+            # add info to dicts
+            if split != 'restval':
+                full_dict[split]['image_id'].append(image_id)
+                full_dict[split]['caption_id'].append(caption_id)
+                full_dict[split]['caption'].append(raw_caption)
+                full_dict[split]['tokens'].append(tokens)
+
+    train_df = pd.DataFrame(train_dict, columns=columns)
+    test_df = pd.DataFrame(test_dict, columns=columns)
+    val_df = pd.DataFrame(val_dict, columns=columns)
+
+    save_path = ROOT_PATH.joinpath('data', 'interim')
+
+    train_file = save_path.joinpath(dataset + '_train.csv')
+    test_file = save_path.joinpath(dataset + '_test.csv')
+    val_file = save_path.joinpath(dataset + '_val.csv')
+
+    train_df.to_csv(train_file)
+    test_df.to_csv(test_file)
+    val_df.to_csv(val_file)
+    print("finished job!!")
+
+
+if __name__ == '__main__':
+    dataset_ = 'flickr30k'
+    data_path_ = ROOT_PATH.joinpath('data', 'raw', 'dataset_' + dataset_ + '.json')
+    order_raw_data_and_move_to_interim(data_path_, dataset_)
+
+
---
+++ karpathy_json_structure.py
@@ -0,0 +1,47 @@
+import json
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+
+def shallow_insights_about_dataset(data_path, dataset):
+    with open(data_path, 'r') as json_file:
+        data_dict = json.load(json_file)
+
+    splitnames = {}
+    filepath_names = {}
+    images_with_more_than_five_captions = {'train': {}, 'test': {}, 'val': {}}
+
+    data_images = data_dict['images']
+
+    for image in data_images:
+        image_split = image['split']
+        if image_split not in splitnames.keys():
+            splitnames[image_split] = 1
+        else:
+            splitnames[image_split] += 1
+        if dataset == 'coco':
+            image_filepath = image['filepath']
+            if image_filepath not in filepath_names.keys():
+                filepath_names[image_filepath] = 1
+            else:
+                filepath_names[image_filepath] += 1
+
+        # find images with more than 5 captions
+        num_caps = len(image['sentids'])
+        if image_split != 'restval' and num_caps > 5:
+            if num_caps not in images_with_more_than_five_captions[image_split].keys():
+                images_with_more_than_five_captions[image_split][num_caps] = 1
+            else:
+                images_with_more_than_five_captions[image_split][num_caps] += 1
+    print(splitnames)
+    print(filepath_names)
+
+    for key in images_with_more_than_five_captions.keys():
+        print(key, images_with_more_than_five_captions[key])
+
+
+if __name__ == '__main__':
+    dataset_ = 'flickr30k'
+    data_path_ = ROOT_PATH.joinpath('data', 'raw', 'dataset_' + dataset_ + '.json')
+    shallow_insights_about_dataset(data_path_, dataset_)
