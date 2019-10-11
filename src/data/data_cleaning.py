11c5b574ece32172f957ce80f4d7e6ff10ef233e
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2019-10-11 15:30:36 +0200

    WIP

--- src/data/data_cleaning.py
+++ src/data/data_cleaning.py
@@ -5,7 +5,7 @@ import pandas as pd
 ROOT_PATH = Path(__file__).absolute().parents[2]
 
 
-def basic_data_cleaning(df_path):
+def basic_data_cleaning(df_path, save_path):
     """
     When we deal with text, we generally perform some basic cleaning
     like lower-casing all the words (otherwise“hello” and “Hello” will
@@ -17,35 +17,49 @@ def basic_data_cleaning(df_path):
     # make a new column for the cleaned version of the caption
     # I do this because I want to keep the original version
     caption_df['clean_caption'] = ''
+    caption_df['clean_tokens'] = []
     # corpus of words, {word: occurrence in corpus}
     corpus = {}
-
-    for i in range(len(caption_df)):
-
-
-
-
-
-
     table = str.maketrans('', '', string.punctuation)
-    for key, desc_list in descriptions.items():
-        for i in range(len(desc_list)):
-            desc = desc_list[i]
-            # tokenize
-            desc = desc.split()
-            # convert to lower case
-            desc = [word.lower() for word in desc]
-            # remove punctuation from each token
-            desc = [w.translate(table) for w in desc]
-            # remove hanging 's' and 'a'
-            desc = [word for word in desc if len(word) > 1]
-            # remove tokens with numbers in them
-            desc = [word for word in desc if word.isalpha()]
-            # store as string
-            desc_list[i] = ' '.join(desc)
+    for i in range(len(caption_df)):
+        cap_tokens = caption_df.loc[i, 'tokens']
+        # convert to lower
+        cap_tokens = [word.lower() for word in cap_tokens]
+        # remove punctuation from each token
+        cap_tokens = [word.translate(table) for word in cap_tokens]
+        # remove hanging 's'
+        cap_tokens = [word for word in cap_tokens
+                      if len(word) > 1 and word != 'a']
+        # remove tokens with numbers in them
+        cap_tokens = [word for word in cap_tokens if word.isalpha()]
+
+        # add words to corpus
+        for word in cap_tokens:
+            if word not in corpus.keys():
+                corpus[word] = 1
+            else:
+                corpus[word] += 1
+
+        # add the cleaned caption to df
+        caption_df.loc[i, 'clean_tokens'] = cap_tokens
+        caption_df.loc[i, 'clean_caption'] = ''.join(cap_tokens)
+    print("Full vocabulary size:", len(corpus))
+
+    # replace words with less than 3 occurrences with a UNK token
+    replace_corpus = [key for key in corpus if corpus[key] < 3]
+    for i in range(len(caption_df)):
+        cap_tokens = caption_df.loc[i, 'clean_tokens']
+        cap_tokens = [word if word not in replace_corpus else 'UNK'
+                      for word in cap_tokens]
+        caption_df.loc[i, 'clean_tokens'] = cap_tokens
+
+    print('vocabulary size that will be used:',
+          len(corpus) - len(replace_corpus))
+
+    caption_df.to_csv(save_path)
 
 
-
-
-
-
+if __name__ == '__main__':
+    df_path_ = ROOT_PATH
+    save_path_ = ROOT_PATH
+    basic_data_cleaning(df_path_, save_path_)
