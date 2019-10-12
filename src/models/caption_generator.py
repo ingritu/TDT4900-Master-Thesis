f1db7e38e7880616f177264b26679757cb1e85db
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2019-10-12 19:20:19 +0200

    model architecture from tutorial

--- src/models/caption_generator.py
+++ src/models/caption_generator.py
@@ -1,7 +1,31 @@
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.models import Model
+from keras import Input
 
 
-class CaptionGenerator:
+def simple_tutorial_model(max_length, vocab_size, embedding_dim):
+    # TODO: write functionalty to attach the pre trained embeddings
+    # image feature extractor model
+    inputs1 = Input(shape=(1536,))
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
 
-    def __init__(self):
-        pass
+    # partial caption sequence model
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, embedding_dim, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
 
+    # decoder (feed forward) model
+    decoder1 = add([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+    # merge the two input models
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.summary()
+    return model
