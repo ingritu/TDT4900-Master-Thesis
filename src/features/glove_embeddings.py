96af3f61cab390f5206470eb7a30ea7bb03c2e18
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2019-10-12 19:08:21 +0200

    gloVe embeddings

---
+++ src/features/glove_embeddings.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def load_glove_vectors(glove_path):
+    # Load Glove vectors
+    embeddings_index = {}  # empty dictionary
+    with open(glove_path, encoding='utf-8') as f:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+    return embeddings_index
+
+
+def embeddings_matrix(vocab_size, wordtoix, embeddings_index):
+    embedding_dim = 500
+    # Get 200-dim dense vector for each of the 10000 words in out vocabulary
+    embedding_matrix = np.zeros((vocab_size, embedding_dim))
+    for word, i in wordtoix.items():
+        # if i < max_words:
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # Words not found in the embedding index will be all zeros
+            embedding_matrix[i] = embedding_vector
+    return embedding_matrix
