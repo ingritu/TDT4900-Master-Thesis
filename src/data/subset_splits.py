c7bfe1b61811f0844097db507e76d4c7e1a9a72e
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2020-01-16 14:55:35 +0100

    outline for subset splits

--- src/data/subset_splits.py
+++ src/data/subset_splits.py
@@ -4,3 +4,15 @@ Main file for creating the dataset splits, for the data exploration.
 
 
 """
+
+
+# load cleaned training dataset
+
+# remove images with less than 5 captions
+
+# remove extra captions if some images have more than 5 captions
+
+# create initial subset with 1 caption per image
+
+# create the next and so on adding an extra caption per image for each itr 
+
