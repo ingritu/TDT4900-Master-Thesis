74349022c4aac6c06659f891585ac6d4e3f78648
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2020-02-28 14:34:01 +0100

    WIP

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -8,9 +8,12 @@ ROOT_PATH = Path(__file__).absolute().parents[2]
 
 
 if __name__ == '__main__':
-    # TODO: argparsing
+    """
+    To run script in terminal.
+    python3 -m src.models.train_model --args
+    """
+    # All default values are the values used in the knowing when to look paper
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--batch_size', type=int, default=256,
                         help='Training batch size. '
                              'The number of captions in a batch.')
@@ -23,8 +26,34 @@ if __name__ == '__main__':
                         help='Beam size to use in beam search '
                              'inference algorithm. '
                              'Bigger beam size yields higher performance.')
-
-    args = parser.parse_args()
+    parser.add_argument('--epochs', type=int, default=50,
+                        help='The number of epochs to train the network for.')
+    parser.add_argument('--embedding_size', type=int, default=512,
+                        help='Embedding dimension. '
+                             'The size of the word vector representations.')
+    parser.add_argument('--hidden_size', type=int, default=512,
+                        help='Hidden dimension.')
+    parser.add_argument('--loss_function', type=str, default='cross_entropy',
+                        help='Loss/Cost function to use during training.')
+    parser.add_argument('--optimizer', type=str, default='adam',
+                        help='Optimizer to use during training.')
+    parser.add_argument('--lr', type=float, default=0.0005,
+                        help='Initial learning rate for the decoder.')
+    parser.add_argument('--seed', type=int, default=222,
+                        help='Random state seed.')
+    parser.add_argument('--model', type=str, default='adaptive_decoder',
+                        help='Model name. Which model type to train.')
+    parser.add_argument('--karpathy', type=bool, default=True,
+                        help='Boolean used to decide whether to train on '
+                             'the karpathy split of dataset or not.')
+    parser.add_argument('--dataset', type=str, default='coco',
+                        help='Dataset to train on. The options are '
+                             '{flickr8k, flickr30k, coco}.')
+    parser.add_argument('--image_feature_size', type=int,
+                        choices=range(2048), nargs='+',
+                        help='List integers. Should be something like '
+                             '--image_feature_size 8 8 1536.')
+    args = vars(parser.parse_args())  # access args as dictionary
 
     train_path = ROOT_PATH.joinpath('data',
                                     'interim',
@@ -52,24 +81,28 @@ if __name__ == '__main__':
 
     save_path_ = ROOT_PATH.joinpath('models')
 
-    model_name_ = 'adaptive_decoder'
+    model_name_ = args['model']
 
-    batch_size = 300  # number of captions in a batch, not images
-    # number of images in a batch, not captions during val
-    val_batch_size = 250
-    beam_size = 3
+    batch_size = args['batch_size']
+    val_batch_size = args['val_batch_size']
+    beam_size = args['beam_size']
 
-    epochs = 10
-    em_dim = 50
-    hidden_size_ = 50
-    loss_function_ = 'cross_entropy'
-    opt = 'adam'
-    lr_ = 0.0001
-    seed_ = 222
+    epochs = args['epochs']
+    em_dim = args['embedding_size']
+    hidden_size_ = args['hidden_size']
+    loss_function_ = args['loss_function']
+    opt = args['optimizer']
+    lr_ = args['lr']
+    seed_ = args['seed']
 
     max_length = max_length_caption(train_path)
 
-    input_shape_ = [[8, 8, 1536], max_length]
+    image_feature_size = args['image_feature_size']
+    assert len(image_feature_size) == 3, "Wrong argument length for " \
+                                         "image_feature_size. " \
+                                         "Expected 3 but got " + \
+                                         str(len(image_feature_size)) + "."
+    input_shape_ = [image_feature_size, max_length]
 
     generator = Generator(model_name_, input_shape_, hidden_size_,
                           voc_path_, feature_path_,
