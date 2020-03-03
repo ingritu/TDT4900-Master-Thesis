7d3fa05b23e3c48cf2320b238ffbb341b11b6675
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2020-03-03 19:04:16 +0100

    WIP

--- src/data/reduce_images_in_dataset.py
+++ src/data/reduce_images_in_dataset.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import pandas as pd
+
+ROOT_PATH = Path(__file__).absolute().parents[2]
 
 """
 main file for creating splits of the training set where the number of images
@@ -5,14 +9,13 @@ are reduced and not the number of captions per image.
 
 """
 
-# load cleaned training dataset
-
-# remove images with less than 5 captions per image
-
-# remove extra captions if there are images with more than 5 cations per image
+prepped_path = ROOT_PATH.joinpath('data',
+                                  'interim',
+                                  'karpathy_split',
+                                  'coco_sub_prep.csv')
+# load prepped data
+data_df = pd.read_csv(prepped_path)
 
 # reduce the number of images until the total number of captions are roughly
 # equal to the number of total captions of the reduced captions subsets.
-
-
 
