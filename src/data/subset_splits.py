22d3f851e84614910f8300012e81b63ea1c104e0
Ingrid Ravn Turkerud <iturkerud@gmail.com>
2020-03-03 19:45:04 +0100

    WIP

--- src/data/subset_splits.py
+++ src/data/subset_splits.py
@@ -17,6 +17,25 @@ prepped_path = ROOT_PATH.joinpath('data',
 # load prepped data
 data_df = pd.read_csv(prepped_path)
 
+# seed random
+r.seed(SEED)
+d = {}
+
+image_ids = list(set(data_df.loc[:, 'image_id']))
+
+# create pick order
+for image_id in image_ids:
+    indices = [i for i in range(5)]
+    r.shuffle(indices)
+    d[image_id] = indices
+print(d[123277])
+
+C1 = {}
+C2 = {}
+C3 = {}
+C4 = {}
+C5 = {}
+
 # create initial subset with 1 caption per image
 
 # create the next and so on adding an extra caption per image for each itr
