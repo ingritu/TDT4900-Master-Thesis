dbdc36d1199c5a7408d7f0052ef672548bdebfef
ingritu <iturkerud@gmail.com>
2020-06-01 14:29:31 +0200

    should be ready to collect

---
+++ src/visualization/collect_caption_samples.py
@@ -0,0 +1,53 @@
+import argparse
+from pathlib import Path
+import pandas as pd
+
+from src.visualization.sample_image_captions import sample_image_captions
+
+ROOT_PATH = Path(__file__).absolute().parents[2]
+
+if __name__ == '__main__':
+    print('Started collect caption samples script.')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--models', type=str, nargs='+', required=True,
+                        help='List of models to collect '
+                             'generated captions from.')
+    parser.add_argument('--sample-size', type=int, default=20,
+                        help='Number of images to sample from test set.')
+    parser.add_argument('--seed', type=int, default=1234,
+                        help='Random seed to determine which images get '
+                             'picked as part of the sample set.')
+
+    args = vars(parser.parse_args())
+
+    print('Parsed Arguments')
+    for k, v in args.items():
+        print(k, v)
+
+    seed = args['seed']
+    models = args['models']
+    sample_size = args['sample_size']
+
+    processed_path = ROOT_PATH.joinpath('data', 'processed')
+    test_path = processed_path.joinpath('coco_test.csv')
+    save_name = 'TEST_sample_' + str(seed) + '.csv'
+    save_file = processed_path.joinpath(save_name)
+
+    if save_file.is_file():
+        # file already exist and we want to add more model results to it
+        res_df = pd.read_csv(save_file)
+    else:
+        # file does not exist
+        labels = ['model_name', 'model_type', 'dataset',
+                  'image_id', 'image_name', 'caption']
+        res_df = pd.DataFrame(columns=labels)
+
+    res_df = sample_image_captions(models,
+                                   test_path,
+                                   res_df,
+                                   seed,
+                                   sample_size=sample_size)
+
+    # save results 
+    res_df.to_csv(save_file, index=False)
+
--- src/visualization/sample_image_captions.py
+++ src/visualization/sample_image_captions.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import json
+import pandas as pd
+from sklearn.utils import shuffle
+import numpy as np
 
 
 ROOT_PATH = Path(__file__).absolute().parents[2]
@@ -38,5 +41,59 @@ def get_model_trainset(model_path, model_type):
     return data
 
 
-def sample_image_captions(models, seed):
-    pass
+def sample_image_captions(models, test_path, res_df, seed, sample_size=20):
+    """
+    Fetches the captions that models produced on a subset of the test set.
+
+    Parameters
+    ----------
+    models : list.
+        List of model names.
+    test_path : Path or str.
+        Path to the test set.
+    res_df : DataFrame.
+        DataFrame where data is saved.
+    seed : int.
+        Random seed.
+    sample_size : int.
+        Size of sample set. Default is 20.
+
+    Returns
+    -------
+    res_df : DataFrame.
+        Updated res_df.
+    """
+    np.random.seed(seed)  # shuffle is seeded through numpy
+    models_dir = ROOT_PATH.joinpath('models')
+
+    # pick images to fetch captions for
+    test_df = pd.read_csv(test_path)
+    shuff = shuffle(test_df)
+    shuff = shuff.reset_index(drop=True)
+    sample_df = shuff.loc[:sample_size-1, ['image_id', 'image_name']]
+
+    index = len(res_df)
+
+    for model_name in models:
+        model_type = get_model_type(model_name)
+        model_path = models_dir.joinpath(model_name)
+        trainset = get_model_trainset(model_path, model_type)
+
+        for i in range(len(sample_df)):
+            image_id = sample_df.loc[i, 'image_id']
+            image_name = sample_df.loc[i, 'image_name']
+            caption = get_image_caption(model_path, image_id)
+
+            if caption is not None:
+                # add to df
+                res_df.loc[index, 'model_name'] = model_name
+                res_df.loc[index, 'model_type'] = model_type
+                res_df.loc[index, 'dataset'] = trainset
+                res_df.loc[index, 'image_id'] = image_id
+                res_df.loc[index, 'image_name'] = image_name
+                res_df.loc[index, 'caption'] = caption
+
+                index += 1
+
+    res_df = res_df.drop_duplicates(ignore_index=True)
+    return res_df
